87f61b0e6708d2b898cc3536bd758b5909decd8e
daniellee <daniellee@Macintosh.local>
2015-05-10 07:48:27 +0900

    BlockDemo

---
+++ Learning_the_Java_Language/LanguageBasics/expressionStatementsBlocks/BlockDemo.java
@@ -0,0 +1,11 @@
+class BlockDemo {
+  public static void main(String[] args) {
+    boolean condition = true;
+    if(condition) {
+      System.out.println("Condition is true.");
+    }
+    else {
+      System.out.println("Condition is false.");
+    }
+  }
+}
